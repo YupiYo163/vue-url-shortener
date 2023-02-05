b0052412564c64511b9af1bc2fb8ac2c336414b2
yupiyo163 <yupiyo163@gmail.com>
2023-02-05 21:32:07 +0500

    test

---
+++ src/App.vue
@@ -0,0 +1,318 @@
+<template>
+  <div class="app">
+    <header class="header">
+      <div class="header__container container">
+        <a href="index.html" class="header__logo">URL SHORTENER</a>
+      </div>
+    </header>
+    <main class="main">
+      <div class="shorten">
+        <div class="shorten__container container">
+          <h1 class="shorten__title">Paste the URL to be shortened</h1>
+          <div class="shorten__search">
+            <input placeholder="Paste your link" type="text" class="shorten__search-input">
+            <button class="shorten__search-button">Shrink</button>
+          </div>
+          <p class="shorten__subtitle">Url Shortened is a free tool to shorten a URL. Use URL Shortener to create a
+            shortned link and use it anywere</p>
+        </div>
+      </div>
+      <div class="advantages">
+        <div class="advantages__container container">
+          <div class="advantages__item" v-for="advantage in advantages" :key="advantage.title">
+            <div class="advantages__item-img">
+              <img :src="require(`@/assets/${advantage.img}`)" alt="">
+            </div>
+            <div class="advantages__item-title">{{ advantage.title }}</div>
+            <div class="advantages__item-subtitle">{{ advantage.description }}</div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="footer">
+      <div class="footer__container container">
+        <p class="footer__creator">Создатель: Азизбек Абибуллаев</p>
+        <p class="footer__creator">Дизайнер: Фаррух Мирмахмудов</p>
+        <p class="footer__creator">Спонсор: Маершин Глеб</p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      advantages: [
+        { img: "easy.png", title: "EASY", description: "URL SHORTENER IS EASY AND FAST AND EFFECTIVE WAY TO SHORTEN YOUR LONG URLS" },
+        { img: "shortened.png", title: "Shortened", description: "Any links can be shortener with powerful algorithms" },
+        { img: "secure.png", title: "Secure", description: "With hugh level encryption your urls are always safe and secure with https" },
+        { img: "stonks.png", title: "Statistics", description: "You can see stats on how many users have used your shortened urt" },
+        { img: "reliable.png", title: "Reliable", description: "We delete any links which is a potential threat tomalware or viruses" },
+        { img: "devices.png", title: "Devices", description: "Compatible with smartphones, tablets and desktops" },
+      ]
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+
+
+* {
+  padding: 0px;
+  margin: 0px;
+  border: none;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+:focus,
+:active {}
+
+a:focus,
+a:active {}
+
+
+
+a,
+a:link,
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+
+
+aside,
+nav,
+footer,
+header,
+section,
+main {
+  display: block;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  font-size: inherit;
+  font-weight: inherit;
+}
+
+ul,
+ul li {
+  list-style: none;
+}
+
+img {
+  vertical-align: top;
+}
+
+img,
+svg {
+  max-width: 100%;
+  height: auto;
+}
+
+address {
+  font-style: normal;
+}
+
+input,
+textarea,
+button,
+select {
+  font-family: inherit;
+  font-size: inherit;
+  color: inherit;
+  background-color: transparent;
+}
+
+input::-ms-clear {
+  display: none;
+}
+
+button,
+input[type="submit"] {
+  display: inline-block;
+  box-shadow: none;
+  background-color: transparent;
+  background: none;
+  cursor: pointer;
+}
+
+input:focus,
+input:active,
+button:focus,
+button:active {
+  outline: none;
+}
+
+button::-moz-focus-inner {
+  padding: 0;
+  border: 0;
+}
+
+label {
+  cursor: pointer;
+}
+
+legend {
+  display: block;
+}
+
+body {
+  height: 100vh;
+}
+
+#app {
+  height: 100%;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+}
+
+.container {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 0px 20px;
+}
+
+.header {
+  background: #566AB0;
+}
+
+.header__container {
+  height: 90px;
+  display: flex;
+  align-items: center;
+}
+
+.header__logo {
+  font-family: "Roboto", sans-serif;
+  color: #fff;
+  font-size: 22px;
+  font-weight: 500;
+}
+
+.main {
+  flex: 1 1 auto;
+}
+
+.footer {
+  background: #566AB0;
+}
+
+.footer__container {
+  height: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.footer__creator {
+  font-size: 16px;
+  color: #fff;
+  font-family: "Roboto", sans-serif;
+}
+
+.shorten__title {
+  color: #fff;
+  font-size: 25px;
+  font-weight: 700;
+  font-family: "Roboto", sans-serif;
+  margin-bottom: 10px;
+}
+
+.shorten__search-input {
+  width: 70%;
+  background: linear-gradient(180deg, #FFFFFF 65.62%, rgba(211, 211, 211, 0.61) 100%);
+  border-radius: 11px;
+  height: 35px;
+  padding: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+.shorten__search-button {
+  margin-left: 20px;
+  color: white;
+  font-family: "Roboto", sans-serif;
+  font-size: 20px;
+  background: #566AB0;
+  height: 35px;
+  padding: 0 20px;
+  border-radius: 10px;
+}
+
+.shorten__search {
+  margin-bottom: 10px;
+}
+
+.shorten__subtitle {
+  color: #fff;
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+}
+
+.shorten {
+  margin-top: 40px;
+}
+
+.main {
+  background: #222222;
+}
+
+.advantages{
+  margin: 60px 0px;
+}
+
+.advantages__container{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.advantages__item{
+  flex: 0 0 33.3333%;
+  margin: 0 50px 50px 50px;
+}
+
+.advantages__item-subtitle{
+  text-align: center;
+  color: #fff;
+  font-size: 16px;
+  text-transform: uppercase;
+  font-family: "Roboto", sans-serif;
+}
+.advantages__item-title{
+  text-align: center;
+  color: #fff;
+  font-size: 16px;
+  text-transform: uppercase;
+  font-family: "Roboto", sans-serif;
+}
+.advantages__item-img img{
+  margin: 0 auto;
+  display: block;
+}
+
+</style>
